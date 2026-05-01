68c29fceade133ec4f6e70be44f9af0d41af1e97
hechmi <hech2005@gmail.com>
2026-05-01 08:07:47 -0400

    Fix DynamoDB module: use proper HCL syntax

--- modules/dynamodb-table/main.tf
+++ modules/dynamodb-table/main.tf
@@ -1,9 +1,29 @@
-variable "table_name" { type = string }
-variable "hash_key" { type = string }
-variable "range_key" { type = string; default = "" }
-variable "billing_mode" { type = string; default = "PAY_PER_REQUEST" }
-variable "environment" { type = string }
-variable "tags" { type = map(string); default = {} }
+variable "table_name" {
+  type = string
+}
+
+variable "hash_key" {
+  type = string
+}
+
+variable "range_key" {
+  type    = string
+  default = ""
+}
+
+variable "billing_mode" {
+  type    = string
+  default = "PAY_PER_REQUEST"
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "tags" {
+  type    = map(string)
+  default = {}
+}
 
 resource "aws_dynamodb_table" "this" {
   name         = var.table_name
@@ -15,10 +35,21 @@ resource "aws_dynamodb_table" "this" {
     type = "S"
   }
 
-  server_side_encryption { enabled = true }
-  point_in_time_recovery { enabled = true }
+  server_side_encryption {
+    enabled = true
+  }
+
+  point_in_time_recovery {
+    enabled = true
+  }
+
   tags = merge(var.tags, { Environment = var.environment })
 }
 
-output "table_name" { value = aws_dynamodb_table.this.name }
-output "table_arn" { value = aws_dynamodb_table.this.arn }
+output "table_name" {
+  value = aws_dynamodb_table.this.name
+}
+
+output "table_arn" {
+  value = aws_dynamodb_table.this.arn
+}
