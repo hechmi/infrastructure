0826a3f2eaeac38c8acaff258b0ee072907f4b08
hechmi <hech2005@gmail.com>
2026-05-01 08:07:46 -0400

    Fix Lambda module: use proper HCL syntax

--- modules/lambda-function/main.tf
+++ modules/lambda-function/main.tf
@@ -1,10 +1,35 @@
-variable "function_name" { type = string }
-variable "runtime" { type = string; default = "python3.12" }
-variable "handler" { type = string; default = "handler.handler" }
-variable "memory_size" { type = number; default = 256 }
-variable "timeout" { type = number; default = 30 }
-variable "environment" { type = string }
-variable "tags" { type = map(string); default = {} }
+variable "function_name" {
+  type = string
+}
+
+variable "runtime" {
+  type    = string
+  default = "python3.12"
+}
+
+variable "handler" {
+  type    = string
+  default = "handler.handler"
+}
+
+variable "memory_size" {
+  type    = number
+  default = 256
+}
+
+variable "timeout" {
+  type    = number
+  default = 30
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "tags" {
+  type    = map(string)
+  default = {}
+}
 
 resource "aws_lambda_function" "this" {
   function_name = var.function_name
@@ -24,14 +49,22 @@ resource "aws_iam_role" "lambda" {
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
+      Action    = "sts:AssumeRole"
+      Effect    = "Allow"
       Principal = { Service = "lambda.amazonaws.com" }
     }]
   })
   tags = var.tags
 }
 
-output "function_name" { value = aws_lambda_function.this.function_name }
-output "function_arn" { value = aws_lambda_function.this.arn }
-output "role_arn" { value = aws_iam_role.lambda.arn }
+output "function_name" {
+  value = aws_lambda_function.this.function_name
+}
+
+output "function_arn" {
+  value = aws_lambda_function.this.arn
+}
+
+output "role_arn" {
+  value = aws_iam_role.lambda.arn
+}
